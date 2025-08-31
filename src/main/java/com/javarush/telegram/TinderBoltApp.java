f99014b9ab220fbda686a2ec832a3be741a15b88
AcidKoss <acidkoss@gmail.com>
2025-08-31 23:22:46 +0300

    1 commit

---
+++ src/main/java/com/javarush/telegram/TinderBoltApp.java
@@ -0,0 +1,42 @@
+package com.javarush.telegram;
+
+import com.javarush.telegram.ChatGPTService;
+import com.javarush.telegram.DialogMode;
+import com.javarush.telegram.MultiSessionTelegramBot;
+import com.javarush.telegram.UserInfo;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.*;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.util.ArrayList;
+
+public class TinderBoltApp extends MultiSessionTelegramBot {
+    public static final String TELEGRAM_BOT_NAME = "AcidKoss_bot"; //TODO: добавь имя бота в кавычках
+    public static final String TELEGRAM_BOT_TOKEN = "7582583026:AAHObaQmUxb7E-cYYL66zvn0a-axs0smpfk"; //TODO: добавь токен бота в кавычках
+    public static final String OPEN_AI_TOKEN = "chat-gpt-token"; //TODO: добавь токен ChatGPT в кавычках
+
+    public TinderBoltApp() {
+        super(TELEGRAM_BOT_NAME, TELEGRAM_BOT_TOKEN);
+    }
+
+    @Override
+    public void onUpdateEventReceived(Update update) {
+
+        sendTextMessage("Привет");
+
+    }
+
+    public static void main(String[] args) throws TelegramApiException {
+        SLF4JBridgeHandler.removeHandlersForRootLogger();
+        SLF4JBridgeHandler.install();
+
+        java.util.logging.Logger telegramLogger =
+                java.util.logging.Logger.getLogger("org.telegram");
+        telegramLogger.setLevel(java.util.logging.Level.ALL);
+
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(new TinderBoltApp());
+    }
+}
